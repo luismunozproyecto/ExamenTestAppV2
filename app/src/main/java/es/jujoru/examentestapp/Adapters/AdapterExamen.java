1a143f92429abe450debfc10ed8cce0a262c4096
jujoru86 <jujoru86@gmail.com>
2018-04-25 11:22:39 +0200

    Subida V2: 2

---
+++ app/src/main/java/es/jujoru/examentestapp/Adapters/AdapterExamen.java
@@ -0,0 +1,64 @@
+package es.jujoru.examentestapp.Adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+import Clases.Examen;
+import es.jujoru.examentestapp.R;
+import es.jujoru.examentestapp.ViewHolders.ViewHolderExamen;
+
+/**
+ * Created by Nino Ruano on 17/04/2018.
+ */
+
+public class AdapterExamen extends RecyclerView.Adapter<ViewHolderExamen>
+        implements View.OnClickListener {
+
+
+    private ArrayList<Examen> datos;
+    private View.OnClickListener listener;
+
+
+    public AdapterExamen(ArrayList<Examen> datos) {
+        this.datos = datos;
+    }
+
+    @Override
+    public ViewHolderExamen onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+
+        View itemView = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.item_examen, viewGroup, false);
+
+        itemView.setOnClickListener(this);
+        ViewHolderExamen tvh = new ViewHolderExamen(itemView,viewGroup.getContext());
+
+        return tvh;
+    }
+
+    public void setOnClickListener(View.OnClickListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (listener != null)
+            listener.onClick(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolderExamen viewHolder, int pos) {
+        Examen item = datos.get(pos);
+        viewHolder.bindExamen(item);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return datos.size();
+    }
+
+}
